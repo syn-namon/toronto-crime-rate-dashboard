0beee8ded44d7539f95494a38370142c6d48bc74
syn-namon <145807233+syn-namon@users.noreply.github.com>
2025-10-23 21:50:20 -0400

    Adjust chart dimensions in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,8 +171,8 @@ if not bar_chart_df.empty:
         tooltip=['AREA_NAME', 'Year', 'Total_Crimes', 'Data_Type']
     ).properties(
         title=f'Crime Volume by Neighbourhood, {from_year:d} to {to_year:d}',
-        height=500, 
-        width=900
+        height=400, 
+        width=1000
     ).interactive() # Allow zooming/panning
 
     # Set use_container_width=False to respect the fixed width setting
